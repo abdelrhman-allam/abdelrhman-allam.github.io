48cc3d2afb6ea503e7e0d46dd0ae868fecbb13e4
Abdelrhman Allam <abdelrhman.allam@gmail.com>
2020-07-01 17:41:28 +0200

    Enhance Colouring

--- grid-algo-visualizer/render.js
+++ grid-algo-visualizer/render.js
@@ -15,22 +15,38 @@
 
         this.random = function random() {
 
-            const r = Math.floor(Math.random() * 30);
-            const g = Math.floor(Math.random() * 30);
+            const r = 50+Math.floor(Math.random() * 200);
+            const g = 50+Math.floor(Math.random() * 200);
             const b = Math.floor(Math.random() * 255);
-            this.rgba = "rgba("+r+","+g+","+b+""; //rgba(135,26,31,0.4286748831988356)
+            this.rgba = "rgba(" + r + "," + g + "," + b +","; //rgba(135,26,31,0.4286748831988356)
 
             this.board = new Array(row * col);
             for (var j = 0; j < this.board.length; j++) {
-                this.board[j] =  (parseFloat(Math.random()+.1).toFixed(3));
-                
+                this.board[j] = (parseFloat(Math.random()).toFixed(2));
             }
-            
+
             this.render();
         }
+        this.render = function () {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            const board = this.board;
+
+            for (let i = 0; i < board.length; i += 1) {
+                const x = this.size * (i % this.col);
+                const y = this.size * Math.floor(i / this.row);
+                ctx.beginPath();
+                ctx.rect(x, y, this.size, this.size);
+                ctx.fillStyle = this.rgba +  (board[i]) + ")";
+                ctx.fill();
+                ctx.font = "6px";
+                // ctx.fillStyle = "rgba(255,255,255,1)";
+                // ctx.fillText((parseFloat(board[i]) * 100).toFixed(0).toString(), x  , y + (size / 2));
+            }
+        }
+
 
         function sleep(time) {
-            
+
             return new Promise(resolve => setTimeout(resolve, time));
         }
 
@@ -42,15 +58,15 @@
                         this.board[j + 1] = parseFloat(this.board[j]);
                         this.board[j] = tmp;
                     }
-                   
+
                 }
                 await this.drawAndWait();
             }
         };
 
-        this.merge =  async function (l, r) {
+        this.merge = async function (l, r) {
             const a = new Array(l.length + r.length);
-      
+
             let k = 0
             let i = 0
             let j = 0
@@ -76,29 +92,29 @@
                 j = j + 1
                 k = k + 1
             }
-            
-    
+
+
             return a
         }
-        
+
         this._mergeSort = async function (array, n) {
-        
+
             const ln = array.length
             if (ln < 2) {
                 return array
             }
             let mid = Math.ceil(ln / 2)
-            
+
             let l = array.slice(0, mid)
-            let r = array.slice(mid , ln)
-            
+            let r = array.slice(mid, ln)
+
             l = await this._mergeSort(l, n)
-            r = await this._mergeSort(r,n+mid)
+            r = await this._mergeSort(r, n + mid)
             a = await this.merge(l, r)
-            
-            for(let m = 0; m < a.length; m++){
-               this.board[n] = a[m]
-               n++
+
+            for (let m = 0; m < a.length; m++) {
+                this.board[n] = a[m]
+                n++
             }
             await this.drawAndWait();
             return a;
@@ -107,44 +123,48 @@
             await this._mergeSort(this.board, 0, 0);
         }
 
-        this.insertionSort = async function (){
+        this.insertionSort = async function () {
             const arr = this.board
             const ln = arr.length
 
-            for ( let i = 1; i < ln; i++){ // start from 1 to sort before the index
+            for (let i = 1; i < ln; i++) { // start from 1 to sort before the index
                 let v = arr[i] // current value
                 let j = i - 1 // before sorted items
-                while (j >=0 && v < arr[j]){ // test value less then previous item
-                   // await this.drawAndWait();
-                    arr[j+1] = arr[j] // override with values with pervious
+                while (j >= 0 && v < arr[j]) { // test value less then previous item
+                    // await this.drawAndWait();
+                    arr[j + 1] = arr[j] // override with values with pervious
                     j = j - 1 // j to previous index    
                 }
-                arr[j+1] = v // assign to last j + 1
+                arr[j + 1] = v // assign to last j + 1
                 await this.drawAndWait();
             }
 
         }
-
-        this.render = function () {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            const board = this.board;
-
-            for (let i = 0; i < board.length; i += 1) {
-                const x = this.size * (i % this.col);
-                const y = this.size * Math.floor(i / this.row);
-                ctx.beginPath();
-                ctx.rect(x, y, this.size, this.size);
-                ctx.fillStyle = this.rgba + ", " +(board[i])+")";
-                ctx.fill();
-                ctx.font = "6px";
-                // ctx.fillStyle = "rgba(255,255,255,1)";
-                // ctx.fillText((parseFloat(board[i]) * 100).toFixed(0).toString(), x  , y + (size / 2));
-            }
+        this.selectionSort = function selectionSort() {
+            this._selectionSort();
         }
 
-        this.quickSort = async function(){
-            
-            await this._quickSort(this.board, 0, this.board.length-1);
+        this.selectionSort = async function _selectionSort() {
+            let arr= this.board
+            let ln = arr.length
+            for (let i = 0; i < ln; i++) {
+                let l = i
+                for (let j = i+ 1; j < ln; j++) {
+                    if (arr[l] > arr[j])
+                        l = j
+                        
+                }
+                await this.drawAndWait()
+                swap(arr, i, l)
+            }            
+            return arr
+        }
+
+
+
+        this.quickSort = async function () {
+
+            await this._quickSort(this.board, 0, this.board.length - 1);
         }
 
         // const getPivot = function(a, l,h){
@@ -161,35 +181,35 @@
         //     return p
         // }
 
-        function swap(arr, l, r){
+        function swap(arr, l, r) {
             let tmp = arr[l]
-            arr[l] =  arr[r]
+            arr[l] = arr[r]
             arr[r] = tmp;
         }
-        this._partition = async function _partion(arr, low, high){
+        this._partition = async function _partion(arr, low, high) {
             let pv = arr[high]
             let border = low
-            
-            for(let i = low; i < high;i++){
-                if(arr[i]< pv){
+
+            for (let i = low; i < high; i++) {
+                if (arr[i] < pv) {
                     swap(arr, border, i)
-                    border = border +1
-                 
+                    border = border + 1
+
                 }
-          
+
             }
-            
-            swap(arr, border, high)            
+
+            swap(arr, border, high)
             return border
         }
-        
-        this._quickSort = async function _quickSort(arr, low, high){
+
+        this._quickSort = async function _quickSort(arr, low, high) {
             // debugger
-            if (low < high){
+            if (low < high) {
                 let p = await this._partition(arr, low, high);
                 await this.drawAndWait();
                 await this._quickSort(arr, low, p - 1);
-                await this._quickSort(arr,  p + 1, high);
+                await this._quickSort(arr, p + 1, high);
             }
         }
 
